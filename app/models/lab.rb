34f857ab334a46fecbf5f7d758c50b7a62e96172
Ish <Ish@MacBook-Pro-de-Ish.local>
2018-05-06 17:07:38 -0400

    modified lab.rb

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -1,7 +1,7 @@
 class Lab < ApplicationRecord
 	validates :name, presence: true
 	validates :chief_id, presence: true
-	validates :description, presence: true
+	validates :description, presence: false
 
 	def title
 		self.name + ' ' + self.chief_id
